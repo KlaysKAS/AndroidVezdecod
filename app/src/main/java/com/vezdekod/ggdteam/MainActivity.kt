a39465d82467eac197516101e32d4500b4cde162
Долматов Кирилл <breakitedkirill@mail.ru>
2022-06-18 17:11:18 +1000

    menu and data classes

--- app/src/main/java/com/vezdekod/ggdteam/MainActivity.kt
+++ app/src/main/java/com/vezdekod/ggdteam/MainActivity.kt
@@ -4,9 +4,12 @@ import android.graphics.Color
 import android.graphics.ColorFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
 import com.vezdekod.ggdteam.categories.CategoryRecyclerAdapter
 import com.vezdekod.ggdteam.databinding.ActivityMainBinding
+import com.vezdekod.ggdteam.menu.MenuItem
+import com.vezdekod.ggdteam.menu.MenuRecyclerAdapter
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
@@ -27,5 +30,44 @@ class MainActivity : AppCompatActivity() {
                 "Горячие блюда"
             )
         )
+
+        val menuRecycler = binding.mainMenuRecycler
+        menuRecycler.layoutManager = GridLayoutManager(this, 2)
+        menuRecycler.adapter = MenuRecyclerAdapter(
+            listOf(
+                MenuItem(
+                    0,
+                    1,
+                    "Удон",
+                    "Просто Удон",
+                    "",
+                    700,
+                    800,
+                    300,
+                    "г",
+                    10.0,
+                    10.0,
+                    10.0,
+                    10.0
+                ),
+                MenuItem(
+                    1,
+                    1,
+                    "Бубон",
+                    "Просто Бубон",
+                    "",
+                    700,
+                    800,
+                    300,
+                    "г",
+                    10.0,
+                    10.0,
+                    10.0,
+                    10.0
+                )
+            )
+        )
+
+
     }
 }
---
+++ app/src/main/java/com/vezdekod/ggdteam/menu/MenuRecyclerAdapter.kt
@@ -0,0 +1,45 @@
+package com.vezdekod.ggdteam.menu
+
+import android.annotation.SuppressLint
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.vezdekod.ggdteam.R
+
+class MenuRecyclerAdapter(private val menuItem: List<MenuItem>): RecyclerView.Adapter<MenuRecyclerAdapter.MenuViewHolder>() {
+
+    @SuppressLint("ResourceAsColor")
+    inner class MenuViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        private val ivImage: ImageView = itemView.findViewById(R.id.menu_item_card_photo)
+        private val ivSpecials: ImageView = itemView.findViewById(R.id.menu_item_card_specials)
+        private val ivName: TextView = itemView.findViewById(R.id.menu_item_card_name)
+        private val ivWeight: TextView = itemView.findViewById(R.id.menu_item_card_weight)
+        private val ivCost: TextView = itemView.findViewById(R.id.menu_item_card_cost_text)
+
+        @SuppressLint("SetTextI18n")
+        fun bind(item: MenuItem) {
+//            ivImage.setImageBitmap()
+//            ivSpecials.setImageBitmap()
+            ivName.text = item.name
+            ivWeight.text = "${item.measure} ${item.measure_unit}"
+            ivCost.text = "${item.priceCurrent} ₽ ${item.priceOld} ₽"
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MenuViewHolder {
+        val itemView =
+            LayoutInflater.from(parent.context)
+                .inflate(R.layout.menu_item_card, parent, false)
+        return MenuViewHolder(itemView)
+    }
+
+    override fun onBindViewHolder(holder: MenuViewHolder, position: Int) {
+        holder.bind(menuItem[position])
+    }
+
+    override fun getItemCount(): Int = menuItem.size
+
+}
